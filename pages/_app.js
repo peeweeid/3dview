eb33e9aeab85f35026718dba74000722fed65556
peeweeid <p2eweeworld@gmail.com>
2024-07-13 19:43:59 +0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <div id="ui" style={{ position: 'absolute', top: '10px', left: '10px', color: 'white', display: 'none' }}>
+        Text UI
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
---
+++ pages/three-d-scene.js
@@ -0,0 +1,8 @@
+// pages/three-d-scene.js (atau three-d-scene.tsx)
+import ThreeDScene from '../components/ThreeDScene';
+
+const ThreeDScenePage = () => {
+  return <ThreeDScene />;
+};
+
+export default ThreeDScenePage;
