a38c9646183b1b52e53954ffc1e1e343120621c8
peeweeid <p2eweeworld@gmail.com>
2024-07-13 19:58:12 +0700

    Fix unescaped entities in JSX

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ import styles from '../styles/index.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1 className='center'>Welcome to My 3D Web Application!</h1>
-      <p>Explore our 3D scene and embrace the "go green" theme:</p>
+      <h1 className='center'>Welcome to My 3D Web Application</h1>
+      <p>Explore our 3D scene and embrace the theme:</p>
       <Link href="/three-d-scene" >
         Go to 3D Scene
       </Link>
